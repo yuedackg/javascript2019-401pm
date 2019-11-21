894b18e2f10ca56f1096c504c9d26d06733e4bd0
Yoshihiro UEDA <cqb00504.00@gmail.com>
2019-11-21 17:20:33 +0900

    201911211720

---
+++ js/18384.js
@@ -0,0 +1,22 @@
+function drawMesg(x){
+ for(k=1;k<=200;k++){
+     document.write("hello<br>");
+ }
+}
+function foo(x){
+    if(x==1){
+        return 1;
+    }else{
+        return x * foo ( x - 1 );
+
+    }
+}
+function bar(p,q){
+    answer=1;
+    for(i=1;i<=q;i++){
+        answer=answer*p;
+        p--;
+        
+    }
+    return answer;
+}
